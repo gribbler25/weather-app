1d4083abf0e07c0e71414f8a569dbbad447860cd
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-07-10 15:51:46 -0500

    refactored code to allow full day forecast to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,13 +43,12 @@ function loadDate() {
   var date5 = document.querySelector("#date5");
   date5.textContent = today5;
 }
-loadDate();
 
 var searchCity = function (cityInput) {
   //use city name to get lat/ long data
   document.getElementById("buttons").innerHTML = ""; //clear the prior buttons
   var urlAPI1 =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
     cityInput +
     "&appid=5b5c4ddd711eb9c884bd57dd6cff32bd";
   //use lat/long data to get the rest of the data from this call:
@@ -57,22 +56,13 @@ var searchCity = function (cityInput) {
     var response = response.json().then(function (data) {
       var city = data[0].name;
       searchTextEl.textContent = city;
-      console.log(city);
-      array.push(city);
-      console.log(array);
-      localStorage.setItem("cities", JSON.stringify(array));
-      var array2 = JSON.parse(localStorage.getItem("cities"));
-      //   if (btnEl.clicked == true) {//this check for submit button status not working
-      //     console.log("clicked!");
-      //   }
-      //create the buttons with city names from local storage array..
-      for (var i = 0; i < array2.length; i++) {
-        var button1 = document.createElement("button");
-        button1.textContent = array2[i];
-        console.log(button1.textContent);
-        $(button1).addClass("coolBtn");
-        savecityEl.appendChild(button1);
+      if (!array.includes(city)) {
+        array.push(city);
+
+        localStorage.setItem("cities", JSON.stringify(array));
       }
+      createButtons();
+
       //get lat/ long for call to API for all stats..
       var latitude = data[0].lat;
       var longitude = data[0].lon;
@@ -87,7 +77,7 @@ var searchCity = function (cityInput) {
           var icono = data.current.weather[0].icon;
           iconEl.setAttribute(
             "src",
-            "http://openweathermap.org/img/w/" + icono + ".png"
+            "https://openweathermap.org/img/w/" + icono + ".png"
           );
           var temp = Math.round(data.current.temp);
           tempEl.textContent = "Temp:  " + temp + "F";
@@ -101,26 +91,47 @@ var searchCity = function (cityInput) {
           if (uv > 6) {
             $(uvEl).removeClass(".today-uv").addClass("red");
           }
-          // for(var i = 1; i < 6; i++) {
+          for (var i = 1; i < 6; i++) {
+            var temp5 = data.daily[i].temp.day;
 
-          var temp5 = data.daily[1].temp.day;
+            var hum5 = data.daily[i].humidity;
 
-          var hum5 = data.daily[1].humidity;
+            var wind5 = data.daily[i].wind_speed;
 
-          var wind5 = data.daily[1].wind_speed;
-
-          var listEl1 = document.querySelector("[data-id='1'] :nth-child(1)");
-          console.log(listEl1);
-          var listEl2 = document.querySelector("[data-id='1'] :nth-child(2)");
-          var listEl3 = document.querySelector("[data-id='1'] :nth-child(3)");
-          listEl1.textContent = "High Temp:  " + temp5 + "F";
-          listEl2.textContent = "Humidity:  " + hum5 + "%";
-          listEl3.textContent = "Wind:  " + wind5 + " MPH";
-          // }
+            var listEl1 = document.querySelector(
+              "[data-id='" + i + "'] :nth-child(1)"
+            );
+            console.log(listEl1);
+            var listEl2 = document.querySelector(
+              `[data-id='${i}'] :nth-child(2)`
+            );
+            var listEl3 = document.querySelector(
+              `[data-id='${i}'] :nth-child(3)`
+            );
+            listEl1.textContent = "High Temp:  " + temp5 + "F";
+            listEl2.textContent = "Humidity:  " + hum5 + "%";
+            listEl3.textContent = "Wind:  " + wind5 + " MPH";
+          }
         });
       });
     });
   });
+};
+//create the buttons with city names from local storage array..
+var createButtons = function () {
+  savecityEl.innerHTML = "";
+  for (var i = 0; i < array.length; i++) {
+    var button1 = document.createElement("button");
+    button1.textContent = array[i];
+    console.log(button1.textContent);
+    $(button1).addClass("coolBtn");
+    savecityEl.appendChild(button1);
+  }
+};
+var load = function () {
+  array = JSON.parse(localStorage.getItem("cities"));
+  loadDate();
+  createButtons();
 };
 
 var savedSearchHandler = function (event) {
@@ -144,6 +155,8 @@ var inputSubmitHandler = function (event) {
 $(".btn").on("click", inputSubmitHandler);
 
 $("#buttons").on("click", savedSearchHandler); //event listener for the ul element where cities are saved
+
+load();
 
 //this loop almost worked, something wrong with substituting 'i' for the data-id.. needs re-working
 //   for (var i = 1; i < 6; i++) {
